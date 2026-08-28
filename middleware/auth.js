acd530b920a1b741731de630e1f33f944d30d62e
kimzy-art <hazeltaylor54321@gmail.com>
2026-08-28 15:17:05 +0100

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -9,7 +9,7 @@ const verifyToken = (req, res, next) => {
   const token = authHeader.split(' ')[1];
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
+    req.user = decoded; // { id, email }
     next();
   } catch (err) {
     return res.status(401).json({ message: 'Unauthorized: Invalid token' });
